3e201d4faa510617d85d10e9c9eb31c2ca02d298
Farhan Rizki <farhanrizki101010@gmail.com>
2023-10-06 16:14:01 +0700

    tambah1

--- js/croot.js
+++ js/croot.js
@@ -3,15 +3,6 @@ import {onClosePopupClick,onDeleteMarkerClick,onSubmitMarkerClick,onMapClick,onM
 import {onClick} from 'https://jscroot.github.io/element/croot.js';
 import {getAllCoordinates} from './controller/cog.js';
 
-// onClick('popup-closer', onClosePopupClick);
-// onClick('insertmarkerbutton', onSubmitMarkerClick);
-// onClick('hapusbutton', onDeleteMarkerClick);
-// onClick('hitungcogbutton', getAllCoordinates);
-
-// map.on('click', onMapClick);
-// map.on('pointermove', onMapPointerMove);
-// map.on('movestart', disposePopover);
-
 document.addEventListener("DOMContentLoaded", () => {
     const pointTable = document.getElementById("pointTable").getElementsByTagName('tbody')[0];
 
@@ -78,110 +69,8 @@ document.addEventListener("DOMContentLoaded", () => {
         .catch(error => console.error("Terjadi kesalahan:", error));
 });
 
-// document.addEventListener('DOMContentLoaded', () => {
-//     const map = new ol.Map({
-//         target: 'map',
-//         layers: [
-//             new ol.layer.Tile({
-//                 source: new ol.source.OSM()
-//             })
-//         ],
-//         view: new ol.View({
-//             center: ol.proj.fromLonLat([106.83303856987703, -6.479261060743909]),
-//             zoom: 15.3
-//         })
-//     });
-
-
-//     // Mendownload data waypoint, line string, dan polyline
-//     const waypointSource = new ol.source.Vector({
-//         url: 'waypoint.json',
-//         format: new ol.format.GeoJSON()
-//     });
-
-//     const lineStringSource = new ol.source.Vector({
-//         url: 'polyline.json',
-//         format: new ol.format.GeoJSON()
-//     });
-
-//     const polylineSource = new ol.source.Vector({
-//         url: 'polygon.json',
-//         format: new ol.format.GeoJSON()
-//     });
-
-//     // Membuat layer untuk waypoint, line string, dan polyline
-//     const waypointLayer = new ol.layer.Vector({
-//         source: waypointSource,
-//         style: new ol.style.Style({
-//             image: new ol.style.Circle({
-//                 radius: 5,
-//                 fill: new ol.style.Fill({
-//                     color: 'blue'
-//                 })
-//             })
-//         })
-//     });
-
-//     const lineStringLayer = new ol.layer.Vector({
-//         source: lineStringSource,
-//         style: new ol.style.Style({
-//             stroke: new ol.style.Stroke({
-//                 color: 'green',
-//                 width: 2
-//             })
-//         })
-//     });
-
-//     const polylineLayer = new ol.layer.Vector({
-//         source: polylineSource,
-//         style: new ol.style.Style({
-//             stroke: new ol.style.Stroke({
-//                 color: 'black',
-//                 width: 2
-
-//             })
-//         })
-//     });
-
-//     // Menambahkan layer ke peta
-//     map.addLayer(waypointLayer);
-//     map.addLayer(lineStringLayer);
-//     map.addLayer(polylineLayer);
-
-//     // Mendapatkan koordinat dari GeoJSON
-//     const getCoordinates = (source) => {
-//         const features = source.getFeatures();
-//         const coordinates = features[0].getGeometry().getCoordinates();
-//         return coordinates;
-//     };
-
-//     // Menampilkan koordinat di dalam tabel
-//     waypointSource.once('change', () => {
-//         const waypointCoords = getCoordinates(waypointSource);
-//         document.getElementById('featureName').textContent = 'Waypoint';
-//         document.getElementById('featureType').textContent = 'Point';
-//         document.getElementById('featureCoords').textContent = waypointCoords.toString();
-//     });
-
-//     lineStringSource.once('change', () => {
-//         const lineStringCoords = getCoordinates(lineStringSource);
-//         document.getElementById('featureName').textContent = 'Line String';
-//         document.getElementById('featureType').textContent = 'Line String';
-//         document.getElementById('featureCoords').textContent = lineStringCoords.toString();
-//     });
-
-//     polylineSource.once('change', () => {
-//         const polylineCoords = getCoordinates(polylineSource);
-//         document.getElementById('featureName').textContent = 'Polyline';
-//         document.getElementById('featureType').textContent = 'Polyline';
-//         document.getElementById('featureCoords').textContent = polylineCoords.toString();
-//     });
-// });
-
 import VectorSource from 'https://cdn.skypack.dev/ol/source/Vector.js';
-import {
-    Vector as VectorLayer
-} from 'https://cdn.skypack.dev/ol/layer.js';
+import {Vector as VectorLayer} from 'https://cdn.skypack.dev/ol/layer.js';
 import GeoJSON from 'https://cdn.skypack.dev/ol/format/GeoJSON.js';
 
 // Definisikan URL GeoJSON untuk masing-masing jenis fitur
